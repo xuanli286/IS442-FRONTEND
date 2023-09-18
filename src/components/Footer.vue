87d377dba5e2e85126fba16dd189cc1361dad490
Regine <regine.sioh.2021@smu.edu.sg>
2023-09-18 23:25:27 +0800

    responsive?

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,9 @@
   <div class="footer">
     <div class="grid grid-cols-4 gap-y-10 gap-x-7 w-[190px]">
       <img src="../assets/gs-logo.svg" class="col-span-4 md:w-[160px] w-[140px]" />
-      <img src="../assets/icons/instagram.svg" />
-      <img src="../assets/icons/linkedin.svg" />
-      <img src="../assets/icons/google.svg" />
+      <img class="hidden md:block" src="../assets/icons/instagram.svg" />
+      <img class="hidden md:block" src="../assets/icons/linkedin.svg" />
+      <img class="hidden md:block" src="../assets/icons/google.svg" />
     </div>
     <div class="text-sm">
       <div class="font-bold">USEFUL LINKS</div>
@@ -20,9 +20,14 @@
       <div>FAQ</div>
       <div>Contact Us</div>
     </div>
-    <div class="col-span-2 grid grid-cols-1 gap-y-10">
+    <div class="hidden md:grid lg:col-span-2 grid-cols-1 gap-y-10">
       <img src="../assets/gs-logo.svg" class="md:w-[160px] w-[140px] opacity-0" />
       <div class="text-xs text-graybrown justify-self-end">© 2023 IS442 G1T5</div>
+    </div>
+    <div class="flex gap-4 md:hidden">
+      <img src="../assets/icons/instagram.svg" />
+      <img src="../assets/icons/linkedin.svg" />
+      <img src="../assets/icons/google.svg" />
     </div>
   </div>
 </template>
@@ -37,9 +42,12 @@ export default {
   .footer {
     @apply
     grid
-    grid-cols-5
+    lg:grid-cols-5
+    md:grid-cols-4
     bg-[#f3f3f3]
     py-11
-    px-12;
+    px-12
+    md:gap-y-0
+    gap-y-12
   }
 </style>
