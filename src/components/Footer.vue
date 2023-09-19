0d313c445c01029582827b8950e472e625178f9a
Regine <regine.sioh.2021@smu.edu.sg>
2023-09-20 03:25:36 +0800

    interactive header/footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="footer">
-    <div class="grid grid-cols-4 gap-y-10 gap-x-7 md:w-[180px] w-[190px]">
+    <div class="grid grid-cols-4 md:gap-y-10 gap-x-5 md:w-[160px] w-[190px]">
       <img src="../assets/gs-logo.svg" class="col-span-4 md:w-[150px] w-[140px]" />
-      <img class="hidden md:block" src="../assets/icons/instagram.svg" />
-      <img class="hidden md:block" src="../assets/icons/linkedin.svg" />
-      <img class="hidden md:block" src="../assets/icons/google.svg" />
+      <a href="https://www.linkedin.com/company/goldman-sachs/" target="_blank">
+        <img class="hidden md:block socials" src="../assets/icons/linkedin.svg" />
+      </a>
+      <a href="https://www.instagram.com/goldmansachs/" target="_blank">
+        <img class="hidden md:block socials" src="../assets/icons/instagram.svg" />
+      </a>
+      <a href="https://twitter.com/goldmansachs" target="_blank">
+        <img class="hidden md:block socials" src="../assets/icons/twitter.svg" />
+      </a>
+      <a href="https://www.youtube.com/user/GoldmanSachs" target="_blank">
+        <img class="hidden md:block socials" src="../assets/icons/youtube.svg" />
+      </a>
     </div>
     <div class="text-sm">
       <div class="font-bold">USEFUL LINKS</div>
@@ -24,10 +33,19 @@
       <img src="../assets/gs-logo.svg" class="md:w-[160px] w-[140px] opacity-0" />
       <div class="text-xs text-graybrown justify-self-end">© 2023 IS442 G1T5</div>
     </div>
-    <div class="flex gap-4 md:hidden">
-      <img src="../assets/icons/instagram.svg" />
-      <img src="../assets/icons/linkedin.svg" />
-      <img src="../assets/icons/google.svg" />
+    <div class="flex w-[170px] gap-4 md:hidden">
+      <a href="https://www.linkedin.com/company/goldman-sachs/" target="_blank">
+        <img class="socials" src="../assets/icons/linkedin.svg" />
+      </a>
+      <a href="https://www.instagram.com/goldmansachs/" target="_blank">
+        <img class="socials" src="../assets/icons/instagram.svg" />
+      </a>
+      <a href="https://twitter.com/goldmansachs" target="_blank">
+        <img class="socials" src="../assets/icons/twitter.svg" />
+      </a>
+      <a href="https://www.youtube.com/user/GoldmanSachs" target="_blank">
+        <img class="socials" src="../assets/icons/youtube.svg" />
+      </a>
     </div>
     <div class="text-xs text-graybrown md:hidden">© 2023 IS442 G1T5</div>
   </div>
@@ -44,11 +62,15 @@ export default {
     @apply
     grid
     lg:grid-cols-5
-    md:grid-cols-4
+    md:grid-cols-[1fr,1fr,1fr,auto]
     bg-[#f3f3f3]
     py-11
     px-12
     md:gap-y-0
     gap-y-12
+  }
+  .socials {
+    @apply
+    hover:opacity-50
   }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,55 @@
 <template>
   <div class="header">
-    <img src="../assets/gs-logo.svg" class="md:w-[120px] w-[100px]" />
+    <RouterLink to="/">
+      <img src="../assets/gs-logo.svg" class="md:w-[120px] w-[100px]" />
+    </RouterLink>
     <div class="col"></div>
-    <div>
-      <div class="text-lg font-semibold text-graybrown">Tan Yi Peng</div>
-      <div class="text-xs text-graybrown">Investment Advisor</div>
+    <div class="profile" @click="displayProfileMenu" ref="profile">
+      <div>
+        <div class="text-lg font-semibold text-graybrown">Tan Yi Peng</div>
+        <div class="text-xs text-graybrown">Investment Advisor</div>
+      </div>
+      <img src="../assets/pfp.png"/>
+      <img class="transition" :class="{ 'chevDown': isActive }" src="../assets/icons/chevron-down-s.svg"/>
     </div>
-    <img src="../assets/pfp.png"/>
-    <img src="../assets/icons/chevron-down-s.svg"/>
   </div>
+  <ul class="dropdown" :class="{ 'active': isActive }">
+    <li class="option">
+      <i class="bi bi-person-circle pr-2"></i>
+      My Profile
+    </li>
+    <li class="option">
+      <i class="bi bi-briefcase-fill pr-2"></i>
+      My Portfolios
+    </li>
+    <hr/>
+    <li class="option">
+      <i class="bi bi-box-arrow-right pr-2"></i>
+      Log out
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'HeaderBlock',
+  data() {
+    return {
+      isActive: false,
+    }
+  },
+  mounted() {
+    document.addEventListener('click', (event) => {
+      if (this.$refs.profile && !this.$refs.profile.contains(event.target)) {
+        this.isActive = false;
+      }
+    });
+  },
+  methods: {
+    displayProfileMenu() {
+      this.isActive = !this.isActive;
+    }
+  },
 }
 </script>
 
@@ -22,11 +58,55 @@ export default {
     @apply
     grid
     grid-rows-1
-    grid-cols-[auto,1fr,auto,auto,auto]
+    grid-cols-[auto,1fr,auto]
     gap-3
     items-center
     bg-[#fff]
     py-7
     px-12
+  }
+  .profile {
+    @apply
+    grid
+    grid-cols-[auto,auto,auto]
+    gap-3
+    items-center
+    hover:bg-gray-50
+    -m-5
+    p-5
+    rounded-lg
+    cursor-pointer
+    relative
+  }
+  .chevDown {
+    @apply
+    rotate-180
+  }
+  .dropdown {
+    @apply
+    absolute
+    px-4
+    md:top-[108.938px]
+    top-[101px]
+    right-0
+    w-[265px]
+    bg-white;
+    overflow: hidden;
+    max-height: 0;
+    transition: max-height 0.2s ease-in-out, padding 0.2s ease-in-out, border 0.2s ease-in-out;
+  }
+  .dropdown.active {
+    max-height: 200px;
+    @apply
+    py-4
+    border
+    border-gray-200;
+  }
+  .option {
+    @apply
+    p-4
+    hover:bg-gray-100
+    cursor-pointer
+    rounded-md
   }
 </style>
