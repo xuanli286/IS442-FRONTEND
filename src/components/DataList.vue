4b80198cd0d939f102713e3d94ced07196822883
Regine <regine.sioh.2021@smu.edu.sg>
2023-09-21 08:37:46 +0800

    datalist

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -1,43 +1,66 @@
 <template>
   <div class="relative">
-    <!-- <i v-if="inputTxt && isActive" class="bi bi-x-circle-fill input-icon transition z-[2]" @click="clear"></i> -->
+    <i v-if="modelValue && display" class="bi bi-x-circle-fill input-icon z-50 cursor-pointer" @click="clear"></i>
     <i class="bi bi-chevron-down input-icon transition z-[0]" :class="{ 'chevDown': isActive }"></i>
-    <input list="stocks" class="input-datalist relative z-[1]" @focus="isActive = true" @blur="isActive = false" placeholder="Stock Ticker Symbol" @input="updateValue" v-model="inputTxt">
-    <datalist id="stocks" class="input-grey">
-    <option v-for="item of items" :key="item.id">{{ item }}</option>
-    </datalist>
+    <input type="text" class="input-datalist relative z-[1]" @focus="isActive = true" placeholder="Symbol" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" ref="inputField">
+    <ul class="dropdown" :class="{ 'active': isActive }" ref="dropdown">
+      <div v-for="(item, idx) of items.filter(item => item.toLowerCase().includes(modelValue.toLowerCase()))" :key="item.id">
+        <li class="stock-option" @click="isActive=!isActive;selectOption(item);">
+          {{ item }}
+        </li>
+        <div class="divider" v-if="idx != items.length-1"></div>
+      </div>
+    </ul>
   </div>
 </template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+  defineProps(['items', 'modelValue'])
+  defineEmits(['update:modelValue'])
+</script>
 
 <script>
 export default {
   name: 'DataList',
-  props: {
-    items: {
-      type: Array,
-    },
-    modelValue: {
-      type: String,
-    },
-  },
-  setup(props, context) {
-    const updateValue = (event) => {
-      context.emit('update:modelValue', event.target.value);
-    }
-
-    return { updateValue }
-  },
   data(){
     return {
       isActive: false,
-      inputTxt: "",
+      display: false,
     }
   },
-//   methods: {
-//     clear() {
-//       this.inputTxt = "";
-//     },
-//   },
+  mounted() {
+    document.addEventListener('click', (event) => {
+      if (this.$refs.inputField && this.$refs.dropdown && !this.$refs.inputField.contains(event.target) && !this.$refs.dropdown.contains(event.target)) {
+        this.isActive = false;
+      }
+    });
+  },
+  watch: {
+    isActive() {
+      if (!this.isActive) {
+        this.display = false;
+      } else {
+        this.display = true;
+      }
+    },
+  },
+  methods: {
+    clear() {
+      setTimeout(() => {
+        this.$emit('update:modelValue', '');
+        this.$refs.inputField.value = '';
+        this.$refs.inputField.dispatchEvent(new Event('input', { bubbles: true }));
+      }, 200);
+    },
+    selectOption(item) {
+      setTimeout(() => {
+        this.$emit('update:modelValue', item)
+        this.$refs.inputField.value = item;
+        this.$refs.inputField.dispatchEvent(new Event('input', { bubbles: true }));
+      }, 100);
+    }
+  },
 }
 </script>
 
@@ -65,11 +88,74 @@ export default {
     text-navy-950
   }
   .input-datalist:focus {
+    background-color: rgb(0, 0, 0, 0.1);
+    outline: 2px solid;
     @apply
-    outline-none
+    outline-navy-950
+    -outline-offset-2
+    rounded-b-none
   }
   .input-datalist::placeholder {
     @apply
     text-navy-800
   }
+  .dropdown {
+    @apply
+    absolute
+    top-[48px]
+    left-0
+    bg-gray-100
+    w-full
+    text-start
+    z-50
+    border-x-2
+    border-transparent
+    overflow-y-auto;
+    overflow: hidden;
+    max-height: 0;
+    transition: max-height 0.2s ease-in-out, min-height 0.2s ease-in-out, padding 0.2s ease-in-out, border 0.2s ease-in-out;
+  }
+  .divider {
+    @apply
+    h-[1px]
+    bg-[#cbcdcf]
+    mx-3
+  }
+  .dropdown.active {
+    @apply
+    min-h-fit
+    max-h-[153px]
+    border-x-2
+    border-b-2
+    border-navy-950
+    rounded-bl-xl
+    rounded-br-xl
+    overflow-y-auto
+  }
+  .stock-option {
+    @apply
+    cursor-pointer
+    p-3
+  }
+  .stock-option:hover {
+    @apply
+    bg-gray-200
+  }
+
+  ::-webkit-scrollbar {
+    width: 12px;
+  }
+  ::-webkit-scrollbar-track {
+    @apply
+    bg-transparent
+  }
+  ::-webkit-scrollbar-thumb {
+    @apply
+    bg-navy-300
+    rounded-xl
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    @apply
+    bg-navy-200
+}
 </style>
--- src/views/CreatePortfolio.vue
+++ src/views/CreatePortfolio.vue
@@ -2,6 +2,7 @@
   <div>
     <h3 class="text-white mb-8">Create Portfolio</h3>
     <div class="white-card">
+      <!-- Basic Information -->
       <h3 class="text-navy-950 mb-8 font-bold">Basic Information</h3>
 
       <h5 class="text-navy-950 mb-2 font-bold">Portfolio Name</h5>
@@ -11,56 +12,72 @@
       <textarea rows="5" placeholder="Write a short description or strategy about the portfolio." class="input-grey w-full"></textarea>
       
       <h3 class="text-navy-950 my-8 font-bold">Add Stocks</h3>
-      <div class="overflow-x-auto">
-        <table class="navy w-full">
+      
+      <!-- Table -->
+      <div class="grid grid-cols-[auto,1fr]">
+        <table class="navy-1 mb-[16.75px] -mr-[1px]">
           <tr>
             <th>Stock</th>
-            <th>Price (SGD)</th>
-            <th>Quantity</th>
-            <th>Total Price (SGD)</th>
-            <th>Allocation (%)</th>
-            <th>ACTION</th>
-            <th></th>
           </tr>
           <tr v-if="stocks.length == 0">
-            <td colspan="6">No stocks added.</td>
-            <td></td>
+            <td>–</td>
           </tr>
-
           <tr v-for="(stock, idx) of stocks" :key="stock.id">
             <td style="padding:5px!important" class="relative">
-              <DataList :items="items.filter(item => !stockNames.includes(item))" v-model="stock.name" @change="newStock(idx)" />
+              <DataList :items="items.filter(item => (!stockNames.includes(item) || stockNames[idx]) )" :model-value="stock.name" @update:model-value="newValue => stock.name = newValue" @input="newStock(stock.name, idx)" />
             </td>
-
-            <td>{{ stock.price }}</td>
-
-            <td>
-              <i class="bi bi-plus float-left cursor-pointer" @click="stock.qty+=1"></i>
-              {{ stock.qty }}
-              <i class="bi bi-dash float-right cursor-pointer" @click="stock.qty-=1"></i>
-            </td>
-
-            <td>{{ stock.total }}</td>
-
-            <td>{{ Math.round( (stock.total/budget*100)  * 100) / 100 }}</td>
-
-            <td v-if="stock.action=='BUY'" class="green-action">{{ stock.action }}</td>
-            <td v-else class="red-action">{{ stock.action }}</td>
-            
-            <td><i class="bi bi-trash-fill cursor-pointer" @click="deleteRow(idx)"></i></td>
           </tr>
-
         </table>
+        <div class="overflow-x-scroll">
+          <table class="navy w-full">
+            <tr>
+              <th>Price (SGD)</th>
+              <th>Quantity</th>
+              <th>Total Price (SGD)</th>
+              <th>Allocation (%)</th>
+              <th>ACTION</th>
+              <th :class='{hidden: stocks.length == 0}'></th>
+            </tr>
+            <tr v-if="stocks.length == 0">
+              <td colspan="5">No stocks added.</td>
+              <td class="hidden"></td>
+            </tr>
+
+            <tr v-for="(stock, idx) of stocks" :key="stock.id">
+              <td>{{ stock.price }}</td>
+
+              <td>
+                <i class="bi bi-plus float-left cursor-pointer" @click="stock.qty+=1"></i>
+                {{ stock.qty }}
+                <i class="bi bi-dash float-right cursor-pointer" @click="stock.qty-=1"></i>
+              </td>
+
+              <td>{{ stock.total }}</td>
+
+              <td>{{ Math.round( (stock.total/budget*100)  * 100) / 100 }}</td>
+
+              <td v-if="stock.action=='BUY'" class="green-action">{{ stock.action }}</td>
+              <td v-else class="red-action">{{ stock.action }}</td>
+              
+              <td :class='{hidden: stocks.length == 0}'><i class="bi bi-trash-fill cursor-pointer" @click="deleteRow(idx)"></i></td>
+            </tr>
+
+          </table>
+        </div>
       </div>
+
+      <!-- [ + ] -->
       <div class="w-full text-center mt-4">
         <button class="btn-navy-dense" @click="addRow()">+</button>
       </div>
 
+      <!-- Other Add Stocks -->
       <h5 class="text-navy-950 mt-8 mb-2 font-bold">Amount of Capital (SGD)</h5>
       <CapitalInput v-model="budget" class="mb-8"/>
 
       <h5 class="text-navy-950 my-8 font-bold">Remaining Balance: ${{ Math.round( (budget - portfolioTotal)  * 100) / 100 }}</h5>
 
+      <!-- Create/cancel buttons -->
       <div class="grid grid-cols-3 gap-12">
         <button class="btn-grey">Cancel</button>
         <button class="btn-navy col-span-2">Create Porfolio</button>
@@ -83,7 +100,7 @@ export default {
     return {
       stocks: [],
       stockNames: [],
-      items: ["APPL", "AOS"],
+      items: ["APPL", "AOS", "TSCO", "DDW"],
       nextStockId: 1,
       budget: null,
     }
@@ -114,13 +131,14 @@ export default {
       this.stockNames.splice(idx, 1);
       console.log(this.stocks);
     },
-    newStock(idx) {
+    newStock(name, idx) {
       let stock = this.stocks[idx];
+      this.stockNames[idx] = name;
+      console.log(this.stockNames)
       if (this.items.includes(stock.name)) {
         // update properties of stocks
         stock.price = 10;
         console.log(stock)
-        this.stockNames[idx] = stock.name;
       }
     },
   },
