3799c9a19c235c106e69b8d439623cba00a86735
Regine <regine.sioh.2021@smu.edu.sg>
2023-09-29 21:04:14 +0800

    landing page layout

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="font-inter">
-    <div>
-      <img class="bg-white absolute" src="@/assets/hero.png" alt="">
-      <div class="relative top-14 px-8 sm:px-12 py-11 text-black-50">
+  <div>
+    <div class="relative h-fit bg-gs">
+      <div class="px-8 sm:px-12 py-28 text-black-50">
         <p class="text-sm font-medium">INVESTMENT ADVISORS & FINANCIAL PROFESSIONALS</p>
         <p class="text-black text-5xl font-medium">
           Investment <br>
@@ -14,14 +13,14 @@
         </p>
       </div>
     </div>
-    <div class="px-8 sm:px-12 py-11 mt-32 text-white font-medium">
-      <p class="z-40 mb-8 text-sm">Features</p>
-      <div class="flex justify-center">
-        <div class="mr-40">
+    <div class="px-8 sm:px-12 py-11 my-11 text-white font-medium">
+      <p class="mb-8 text-sm">FEATURES</p>
+      <div class="grid grid-cols-1 gap-y-5 sm:grid-cols-2">
+        <div class="mx-auto">
           <img src="@/assets/chart.png" alt="">
           <p class="mt-5">Interactive Charts and <br> Dashboard for the best risk <br> insights.</p>
         </div>
-        <div>
+        <div class="mx-auto">
           <img src="@/assets/secure.png" alt="">
           <p class="mt-5">Secure Platform for your <br> portfolio management.</p>
         </div>
@@ -33,3 +32,12 @@
 <script setup>
 
 </script>
+
+<style scoped>
+  .bg-gs {
+    background-image: url("@/assets/hero.png");
+    background-color: #fff;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
