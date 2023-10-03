c1ef1f72b88c68cb482815add3175f6e4da20a3c
xuanli286 <xuanli743@gmail.com>
2023-10-04 00:12:10 +0800

    complete line charts & portfolios comparison

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -13,47 +13,61 @@
                     <div class="flex items-center">
                         <p class="font-semibold mr-2">Top Performers</p>
                         <i class="fa fa-line-chart" aria-hidden="true"></i>
-                        <PortfolioButton class="ml-auto" @click="isCompare=!isCompare">
+                        <PortfolioButton class="ml-auto" @click="isCompare = !isCompare">
                             <p>Compare Portfolio</p>
                         </PortfolioButton>
                     </div>
                     <div class="grid grid-cols-3 gap-4 mt-5">
-                        <div class="border-2 border-blue-950 p-3 rounded-lg " v-for="(portfolio, id) of top3Portfolios" :key="id">
+                        <div class="border-2 border-blue-950 p-3 rounded-lg " v-for="(portfolio, id) of top3Portfolios"
+                            :key="id">
                             <p class="font-bold">{{ portfolio.portfolioName }}</p>
-                            <p class="text-gray-500 text-xs">Date Created: {{ (new Date(portfolio.dateCreated)).toLocaleString(undefined, { year: 'numeric', month: 'numeric', day: 'numeric' }) }}</p>
+                            <p class="text-gray-500 text-xs">Date Created: {{ (new
+                                Date(portfolio.dateCreated)).toLocaleString(undefined, {
+                                    year: 'numeric', month: 'numeric',
+                                    day: 'numeric'
+                                }) }}</p>
                             <p class="text-sm font-medium mt-10">Portfolio Value</p>
                             <p class="font-bold flex items-center">
                                 <span class="bg-blue-600 text-white rounded-full p-1 text-xs mr-1">SGD</span>
                                 {{ portfolio.portfolioValue.toLocaleString('en-US', {
-                                        style: 'decimal',
-                                        minimumFractionDigits: 2,
-                                        maximumFractionDigits: 2,
-                                    })
+                                    style: 'decimal',
+                                    minimumFractionDigits: 2,
+                                    maximumFractionDigits: 2,
+                                })
                                 }}
                             </p>
                         </div>
                     </div>
                 </div>
                 <div class="col-span-1 white-card">
-                    <DonutChart :isOverview="true"/>
+                    <DonutChart :isOverview="true" />
                 </div>
                 <div class="col-span-2 white-card">
-                    <p class="font-semibold mt-1">Portfolio Performance</p>
-                    <LineChart :portfoliosValue="portfoliosValue"/>
+                    <p class="font-semibold mt-1 mb-4">
+                        Portfolio Performance for Year 
+                        <span class="font-bold text-lg">{{ currentYear }}</span>
+                    </p>
+                    <LineChart :dataset="dataset" />
+                </div>
+                <div class="col-span-3 white-card">
+                    <p class="font-semibold mt-1">Country Exposure</p>
+                    <MapChart :countryData="{ 'US': 4, 'CA': 7, 'GB': 8, 'IE': 14, 'ES': 21 }" highColor="#192E47"
+                        lowColor="#5CA7FF" countryStrokeColor="#909090" defaultCountryFillColor="#dadada" />
                 </div>
             </div>
         </div>
         <div v-else class="white-card">
             <div class="flex items-center">
-                <i v-if="isHover" class="fa-solid fa-circle-left mr-2 cursor-pointer text-2xl" @mouseleave="isHover=false" @click="back"></i>
-                <i v-else class="fa-regular fa-circle-left mr-2 cursor-pointer text-2xl" @mouseenter="isHover=true"></i>
+                <i v-if="isHover" class="fa-solid fa-circle-left mr-2 cursor-pointer text-2xl" @mouseleave="isHover = false"
+                    @click="back"></i>
+                <i v-else class="fa-regular fa-circle-left mr-2 cursor-pointer text-2xl" @mouseenter="isHover = true"></i>
                 <p class="font-semibold">
                     <span class="mr-2">Compare Portfolios</span>
                     <i class="fa-solid fa-minimize"></i>
                 </p>
             </div>
             <label class="text-sm" v-for="(portfolio, id) of top3Portfolios" :key="id">
-                <input class="ml-8" type="checkbox" v-model="selectedPortfolios" :value="id" :disabled="selectedPortfolios.length >= 2 && !selectedPortfolios.includes(id)">
+                <input class="ml-8" type="checkbox" v-model="selectedPortfolios" :value="id" :disabled="selectedPortfolios.length >= 2 && !selectedPortfolios.includes(id)" @change="handlePortfolioComparison">
                 {{ id }}
             </label>
             <div class="grid grid-cols-2 gap-4 mt-3 mx-8">
@@ -68,30 +82,37 @@
                     <div class="mt-5 flex items-center">
                         <p class="font-semibold">Portfolio Value</p>
                         <p class="ml-auto">{{ top3Portfolios[id].portfolioValue.toLocaleString('en-US', {
-                                    style: 'decimal',
-                                    minimumFractionDigits: 2,
-                                    maximumFractionDigits: 2,
-                                }) }}
+                            style: 'decimal',
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                        }) }}
                         </p>
                     </div>
                     <div class="mt-5 flex items-center">
                         <p class="font-semibold">Unrealised P&L</p>
                         <p class="ml-auto">{{ top3Portfolios[id].unrealisedPnL.toLocaleString('en-US', {
-                                    style: 'decimal',
-                                    minimumFractionDigits: 2,
-                                    maximumFractionDigits: 2,
-                                }) }}
+                            style: 'decimal',
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                        }) }}
                         </p>
                     </div>
                     <div class="mt-5 flex items-center">
                         <p class="font-semibold">Capital Amount</p>
                         <p class="ml-auto">{{ top3Portfolios[id].capital.toLocaleString('en-US', {
-                                    style: 'decimal',
-                                    minimumFractionDigits: 2,
-                                    maximumFractionDigits: 2,
-                                }) }}
+                            style: 'decimal',
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                        }) }}
                         </p>
                     </div>
+                </div>
+                <div v-if="selectedPortfolios.length > 0" class="col-span-2 white-card">
+                    <p class="font-semibold mt-1 mb-4">
+                        Portfolio Performance Comparison for Year 
+                        <span class="font-bold text-lg">{{ currentYear }}</span>
+                    </p>
+                    <LineChart :dataset="selectedDataset" />
                 </div>
             </div>
         </div>
@@ -101,39 +122,97 @@
 <script setup>
     import DonutChart from '@/components/charts/DonutChart.vue';
     import PortfolioButton from '@/components/PortfolioButton.vue';
-    import { defineProps, ref, onMounted, computed } from 'vue';
+    import { defineProps, ref, onMounted, computed, watch } from 'vue';
     import LineChart from '@/components/charts/LineChart.vue';
     import { useUserStore } from "@/stores/useUserStore";
     import { storeToRefs } from 'pinia';
+    // import ChoroplethMap from "@/components/charts/ChoroplethMap.vue";
+    import MapChart from 'vue-map-chart'
+    import axios from 'axios';
+    import { useAuth0 } from '@auth0/auth0-vue';    
+    const { user, isAuthenticated } = useAuth0();
 
     const store = useUserStore();
     const {
         loginUser
     } = storeToRefs(store);
 
-    const props = defineProps({
-        top3Portfolios: {
-            type: Object,
-            required: true,
-        },
-        portfoliosValue: {
-            type: Object,
-            required: true,
-        }
-    });
-
     const isCompare = ref(false);
     const isHover = ref(false);
     const selectedPortfolios = ref([]);
+    const top3Portfolios = ref({});
+    const dataset = ref({});
+    const selectedDataset = ref({});
+    const currentYear = ref("");
 
-    const defaultPortfolios = computed(() => {
-        return Object.keys(props.top3Portfolios).slice(0, 2);
+    currentYear.value = new Date().getFullYear();
+
+    onMounted(async () => {
+        if (isAuthenticated) {
+            try {
+                const response = await axios.get(`http://localhost:5000/portfolio/getportfolios/${user.value.sub}`);
+                response.data.sort((a, b) => b.portfolioValue - a.portfolioValue);
+                const portfolios = response.data;
+                const performingPortfolios = portfolios.slice(0, 3);
+                for (let portfolio of performingPortfolios) {
+                    let key = `${portfolio.portfolioName} [${portfolio.portfolioId}]`;
+                    top3Portfolios.value[key] = {
+                        portfolioId: portfolio.portfolioId,
+                        portfolioName: portfolio.portfolioName,
+                        portfolioValue: portfolio.portfolioValue,
+                        unrealisedPnL: portfolio.unrealisedPnL,
+                        dateCreated: portfolio.dateCreated,
+                        capital: portfolio.capital,
+                    };
+                }
+                selectedPortfolios.value = Object.keys(top3Portfolios.value).slice(0, 2);
+                for (let portfolio of portfolios) {
+                    let totalvalue = new Array(12).fill(0);
+                    for (let i = 1; i <= 12; i++) {
+                        for (let [key, value] of Object.entries(portfolio.portStock)) {
+                            try {
+                                const response = await axios.get(
+                                    `http://localhost:5000/stockprice/getmonthlypricebydate/${key}?month=${i.toString().padStart(2, '0')}&year=${currentYear.value}`
+                                );
+                                let lastDayOfMonth = response.data["stockDate"];
+                                let monthStockPrice = response.data["4. close"];
+                                for (let transaction of value) {
+                                    let [day, month, year] = transaction.dateBought.split('/');
+                                    let dateBought = new Date(`${year}-${month}-${day}`);
+                                    dateBought = dateBought.toISOString();
+                                    if (dateBought <= lastDayOfMonth) {
+                                        totalvalue[i - 1] += (monthStockPrice - transaction.stockBoughtPrice) * transaction.quantity;
+                                    }
+                                }
+                            } catch (error) {
+                                console.error(error.message);
+                            }
+                        }
+                    }
+                    dataset.value[`${portfolio.portfolioName} [${portfolio.portfolioId}]`] = totalvalue;
+                }
+                handlePortfolioComparison();
+            } catch (error) {
+                console.error(error.message);
+            }
+            console.log(dataset.value)
+        }
     });
 
-    onMounted(() => {
-        selectedPortfolios.value = defaultPortfolios.value;
-        // console.log(selectedPortfolios.value)
-    })
+    function handlePortfolioComparison() {
+        selectedDataset.value = {};
+        for (let portfolioName of selectedPortfolios.value) {
+            for (let [key, value] of Object.entries(dataset.value)) {
+                if (portfolioName == key) {
+                    selectedDataset.value[key] = value;
+                }
+            }
+        }
+    }
+
+    watch(selectedPortfolios, () => {
+        handlePortfolioComparison();
+    });
 
     function back() {
         isCompare.value = false;
