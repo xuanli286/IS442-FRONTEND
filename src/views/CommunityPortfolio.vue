45a84869b5180dcdd5428642b0e62f4ed4dca968
Regine <regine.sioh.2021@smu.edu.sg>
2023-10-15 04:55:02 +0800

    communityPortfolio skeleton

--- src/views/CommunityPortfolio.vue
+++ src/views/CommunityPortfolio.vue
@@ -1,115 +1,185 @@
 <template>
-  <div class="px-8 sm:px-12 py-11">
-    <h3 class="text-white mb-8">Community Porfolios</h3>
-    <!-- Search & Filter -->
-    <div class="pb-10 grid grid-cols-[1fr,auto] md:grid-cols-[60%,auto,1fr] lg:grid-cols-[45%,auto,1fr] gap-8">
-      <SearchInput v-model="query"/>
-      <button class="btn-outline-white" @click="isModal=true">Filter</button>
-      <div class="hidden md:block"></div>
-    </div>
-    <div class="white-card mb-10" v-for="(p, idx) of filteredData.slice( (page-1)*5, page*5 )">
-      <div class="font-bold mb-2">{{ p.portfolioName }}</div>
-      <div class="text-sm mb-7">Created by: {{ p.username }}, {{ p.dateCreated }}</div>
-      <div class="">Portfolio Value: ${{ p.portfolioValue }}</div>
-    </div>
+    <div class="px-8 sm:px-12 py-11">
+        <h3 class="text-white pb-11">
+            Portfolio
+        </h3>
+        <div class="white-card">
+            <div class="flex justify-between">
+                <div>
+                    <p class="my-4 text-gray-700">Created by: </p>
+                    <p class="my-4 text-gray-700">Date created: </p>
+                    <p class="my-4 text-gray-700">Portfolio Description: </p>
+                    <p class="my-4 text-gray-700">Capital: </p>
+                    <p class="my-4 text-gray-700">Unrealised PnL: </p>
+                </div>
+                <div class="float-right">$$$$</div>
+            </div>
+            <div class="overflow-x-auto">
+                <table class="w-full">
+                    <tr>
+                        <th>Stock</th>
+                        <th>Sector</th>
+                        <th>Price</th>
+                        <th>Allocation</th>
+                    </tr>
+                    <tr v-for="log of filteredData.slice( (page-1)*6, page*6 )">
+                        <td>{{ formatDate(log.dateTime) }}</td>
+                        <td>{{ log.action }}</td>
+                        <td>{{ log.info }}</td>
+                        <td></td>
+                    </tr>
+                </table>             
+            </div>
+        </div>
 
-    <!-- Pagination -->
-    <Pagination class="mb-10 float-right" :maxPage="Math.ceil(filteredData.length/5)" v-model="page"/>
+        <Pagination class="mb-10 mt-7 float-right" :maxPage="Math.ceil(filteredData.length/6)" v-model="page"/>
 
-    <!-- Modal -->
-    <Modal v-model="isModal" width="50%" height="fit-content">
-      <h3 class="text-navy-950 mb-8">Sort By</h3>
-      <div v-for="(condition, ppty, idx) in conditions" class="mb-2">
-        <label>
-          <input type="radio" v-model="option" name="sortby" :value="ppty"> {{ condition.name }}
-        </label>
-        <i class="pl-3 text-lg bi cursor-pointer" :class="{'bi-sort-up': condition.ascending, 'bi-sort-down': !condition.ascending}" @click="condition.ascending = !condition.ascending"></i>
-      </div>
-      <button class="btn-navy mt-6" style="font-weight:normal !important" @click="clearConditions">Clear</button>
-    </Modal>
-  </div>
+    </div>
 </template>
 
 <script>
-import SearchInput from '../components/SearchInput.vue'
-import Pagination from '../components/Pagination.vue'
-import Modal from '../components/Modal.vue'
+import { useUserStore } from "@/stores/useUserStore";
 import axios from "../axiosConfig";
+import { ref } from "vue";
+import Pagination from '../components/Pagination.vue'
 
 export default {
-  name: 'CommunityPortfolio',
-  components: {
-    SearchInput,
+    name: "CommunityPortfolio",
+    components: {
     Pagination,
-    Modal,
-  },
-  data(){
-    return {
-      query: "",
-      page: 1,
-      isModal: false,
-      defaultConditions: {
-        dateCreated: {name: "Date Created", ascending: false},
-        portfolioValue: {name: "Portfolio Value", ascending: false},
-      },
-      option: "",
-      conditions: {},
-      portfolios: [],
-    }
-  },
-  created() {
-    this.conditions = JSON.parse(JSON.stringify(this.defaultConditions));
-    this.populate();
-  },
-  computed: {
-    filteredData() {
-      var result = JSON.parse(JSON.stringify(this.portfolios));
-
-      if (this.query) {
-        result = this.portfolios.filter((item) => ( item.portfolioName.toLowerCase().includes(this.query.toLowerCase()) || item.username.toLowerCase().includes(this.query.toLowerCase())));
-      }
-      if (this.option) {
-        if (this.option == "dateCreated") {
-          result = result.sort((a, b) => this.conditions[this.option].ascending ? ( new Date(a[this.option]) - new Date(b[this.option]) ): ( new Date(b[this.option]) - new Date(a[this.option]) ));
-        } else {
-          result = result.sort((a, b) => this.conditions[this.option].ascending ? ( a[this.option] - b[this.option] ): ( b[this.option] - a[this.option] ));
-        }
-      }
-
-      return result;
     },
-  },
-  methods: {
-    async populate() {
-      await axios.get(`http://localhost:5000/portfolio/getpublicportfolios`)
-      .then((response) => {
-        this.portfolios = response.data;
-        for (let i=0; i<this.portfolios.length; i++) {
-          axios.get(`http://localhost:5000/customer/${this.portfolios[i].userId}`)
-          .then((response) => {
-            this.portfolios[i].username = response.data.customerData.name;
-          })
-          .catch((error) => {
-            this.portfolios[i].username = this.portfolios[i].userId;
-          })
-        }
-      })
-      .catch((error) => {
-        console.log(error.message);
-      })
-      console.log(this.portfolios);
+    setup() {
+        const userID = useUserStore().loginUser.id;
+        const testData = ref([]);
+        console.log(userID);
+
+        axios
+            .get(`http://localhost:5000/accesslog/getlogs/${userID}`)
+            .then((response) => {
+                console.log(response.data);
+                testData.value = response.data;
+
+                // action: "CREATE";
+                // dateTime: "2023-10-05T00:04:00.009722300";
+                // info: "Created Portfolio";
+                // ipAddress: "0:0:0:0:0:0:0:1";
+                // success: true;
+                // userId: "google-oauth2|117368012179621015773";
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+
+        return { userID, testData };
     },
-    clearConditions() {
-      this.option = "";
-      this.conditions = {...JSON.parse(JSON.stringify(this.defaultConditions))};
-    }
-  },
-}
+    data() {
+        return {
+            start: "",
+            end: "",
+            page: 1,
+        };
+    },
+    computed: {
+        filteredData() {
+            if (this.start && this.end) {
+                return this.testData.filter(
+                    (item) =>
+                        new Date(item.dateTime.split("T")[0]).setHours(0) >=
+                            new Date(this.start).setHours(0) &&
+                        new Date(item.dateTime.split("T")[0]).setHours(0) <=
+                            new Date(this.end).setHours(0)
+                );
+            } else {
+                return this.testData;
+            }
+        },
+    },
+    watch: {
+        start() {
+            if (this.start && this.end) {
+                if (this.start > this.end) {
+                    this.end = this.start;
+                }
+            }
+        },
+        end() {
+            if (this.start && this.end) {
+                if (this.end < this.start) {
+                    this.end = this.start;
+                }
+            }
+        },
+    },
+    methods: {
+        formatDate(dateTime) {
+            if (!dateTime) return "";
+            const date = new Date(dateTime);
+            const day = date.getDate().toString().padStart(2, "0");
+            const month = (date.getMonth() + 1).toString().padStart(2, "0");
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        }
+    },
+};
 </script>
 
 <style scoped>
-  h3 {
-    @apply
-    text-2xl
-  }
+h3 {
+    @apply text-2xl;
+}
+
+.white-card {
+    @apply bg-white
+    rounded-md
+    px-8
+    py-12;
+}
+
+input::-webkit-datetime-edit-day-field:focus,
+input::-webkit-datetime-edit-month-field:focus,
+input::-webkit-datetime-edit-year-field:focus {
+    @apply bg-[#8ba7ed];
+}
+
+input[type="date"] {
+    @apply rounded-xl
+    border-2
+    border-gray-300
+    p-3
+    text-navy-950;
+}
+
+table {
+    @apply border-x-2
+    border-t-2
+    border-gray-200
+    rounded-xl;
+}
+
+table tr {
+    @apply border-b-2
+    border-gray-200;
+}
+
+table th {
+    @apply font-bold
+    bg-gray-100
+    p-4
+    w-1/4;
+}
+
+table td {
+    @apply p-4
+    text-center;
+}
+
+.badge {
+    @apply py-1
+    font-bold
+    w-[100px]
+    mx-auto
+    rounded-md
+    text-white
+    text-sm;
+}
 </style>
