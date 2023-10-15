bc580f67b650217253785cf7cc9faa577e958fbf
rui <regine.sioh.2021@scis.smu.edu.sg>
2023-10-15 18:11:38 +0800

    communityPortfolio

--- src/views/CommunityPortfolio.vue
+++ src/views/CommunityPortfolio.vue
@@ -1,124 +1,59 @@
 <template>
-    <div class="px-8 sm:px-12 py-11">
-        <h3 class="text-white pb-11">
-            Portfolio
-        </h3>
-        <div class="white-card">
-            <div class="flex justify-between">
-                <div>
-                    <p class="my-4 text-gray-700">Created by: </p>
-                    <p class="my-4 text-gray-700">Date created: </p>
-                    <p class="my-4 text-gray-700">Portfolio Description: </p>
-                    <p class="my-4 text-gray-700">Capital: </p>
-                    <p class="my-4 text-gray-700">Unrealised PnL: </p>
-                </div>
-                <div class="float-right">$$$$</div>
-            </div>
-            <div class="overflow-x-auto">
-                <table class="w-full">
-                    <tr>
-                        <th>Stock</th>
-                        <th>Sector</th>
-                        <th>Price</th>
-                        <th>Allocation</th>
-                    </tr>
-                    <tr v-for="log of filteredData.slice( (page-1)*6, page*6 )">
-                        <td>{{ formatDate(log.dateTime) }}</td>
-                        <td>{{ log.action }}</td>
-                        <td>{{ log.info }}</td>
-                        <td></td>
-                    </tr>
-                </table>             
-            </div>
-        </div>
-
-        <Pagination class="mb-10 mt-7 float-right" :maxPage="Math.ceil(filteredData.length/6)" v-model="page"/>
-
+    <div class="mx-8 sm:mx-12 mt-11 mb-8">
+        <h3 class="text-white mb-4">{{ pfData.portfolioName }}</h3>
+        <div class="rounded-full bg-gray-300 w-fit p-2">by {{ creator }}</div>
     </div>
+    <div class="px-8 sm:px-12 pb-11 font-inter">
+        <SummarizedValue :portfolio="pfData" />
+        <Portfolio :portfolio="pfData" />
+    </div>
+    <PortfolioBreakdown :portfolio="pfData"/>
 </template>
 
 <script>
-import { useUserStore } from "@/stores/useUserStore";
 import axios from "../axiosConfig";
 import { ref } from "vue";
-import Pagination from '../components/Pagination.vue'
+import SummarizedValue from '../components/SummarizedValue.vue';
+import Portfolio from "./Portfolio.vue";
 
 export default {
     name: "CommunityPortfolio",
     components: {
-    Pagination,
-    },
-    setup() {
-        const userID = useUserStore().loginUser.id;
-        const testData = ref([]);
-        console.log(userID);
-
-        axios
-            .get(`http://localhost:5000/accesslog/getlogs/${userID}`)
-            .then((response) => {
-                console.log(response.data);
-                testData.value = response.data;
-
-                // action: "CREATE";
-                // dateTime: "2023-10-05T00:04:00.009722300";
-                // info: "Created Portfolio";
-                // ipAddress: "0:0:0:0:0:0:0:1";
-                // success: true;
-                // userId: "google-oauth2|117368012179621015773";
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-
-        return { userID, testData };
+        SummarizedValue,
+        Portfolio
     },
     data() {
         return {
-            start: "",
-            end: "",
+            pID: this.$route.query.pID,
             page: 1,
+            pfData: [],
+            creator: "",
         };
     },
-    computed: {
-        filteredData() {
-            if (this.start && this.end) {
-                return this.testData.filter(
-                    (item) =>
-                        new Date(item.dateTime.split("T")[0]).setHours(0) >=
-                            new Date(this.start).setHours(0) &&
-                        new Date(item.dateTime.split("T")[0]).setHours(0) <=
-                            new Date(this.end).setHours(0)
-                );
-            } else {
-                return this.testData;
-            }
-        },
-    },
-    watch: {
-        start() {
-            if (this.start && this.end) {
-                if (this.start > this.end) {
-                    this.end = this.start;
-                }
-            }
-        },
-        end() {
-            if (this.start && this.end) {
-                if (this.end < this.start) {
-                    this.end = this.start;
-                }
-            }
-        },
+    created() {
+        this.populate();
     },
     methods: {
-        formatDate(dateTime) {
-            if (!dateTime) return "";
-            const date = new Date(dateTime);
-            const day = date.getDate().toString().padStart(2, "0");
-            const month = (date.getMonth() + 1).toString().padStart(2, "0");
-            const year = date.getFullYear();
-            return `${day}/${month}/${year}`;
-        }
+        populate() {
+            axios.get(`http://localhost:5000/portfolio/${this.pID}`)
+            .then((response) => {
+                if (response.data.public) {
+                    this.pfData = response.data;
+                    axios.get(`http://localhost:5000/customer/${response.data.userId}`)
+                    .then((response) => {
+                        this.creator = response.data.customerData.name;
+                        console.log(this.pfData);
+                    })
+                    .catch((error) => {
+                        console.log(error.message);
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log(error.message);
+                this.$router.go(-1);
+            })
+        },
     },
 };
 </script>
