d5698c9a3e4a7416efa21cc4939c74b36b8d7ad9
Germaine <tanlixuangermaine@gmail.com>
2023-10-16 22:45:17 +0800

    update PortfolioBreakdown to take latest update only

--- src/components/PortfolioDropdown.vue
+++ src/components/PortfolioDropdown.vue
@@ -30,7 +30,7 @@ export default {
     },
     props: {
         portfolio: {
-            type: Object,
+            // type: Object,
             required: true,
         }
     },
@@ -54,7 +54,6 @@ export default {
         selectPortfolio(portfolio) {
             this.$emit("isSelect", portfolio);
             this.isOpen = false; // Close the dropdown
-            console.log(portfolio)
         },
 
     },
