0f9b559d4056d45c8f142a716e51d8ff5521cee0
rui <regine.sioh.2021@scis.smu.edu.sg>
2023-10-19 13:19:10 +0800

    display in 2.d.p for user logs and handled no logs case

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="flex space-x-2 w-fit bg-white rounded-md p-2">
-    <button class="btn-white" @click="start">
+    <button class="btn-white" @click="start" v-if="modelValue != 1">
       <i class="bi bi-chevron-bar-left"></i>
     </button>
-    <button class="btn-white" @click="decrement">
+    <button class="btn-white" v-else disabled>
+      <i class="bi bi-chevron-bar-left"></i>
+    </button>
+    <button class="btn-white" @click="decrement" v-if="modelValue > 1">
+      <i class="bi bi-chevron-left"></i>
+    </button>
+    <button class="btn-white" v-else disabled>
       <i class="bi bi-chevron-left"></i>
     </button>
     <input type="number" class="input-search" min="1" :max="maxPage" :value="modelValue" @blur="registerValue" @keydown.enter="registerValue" ref="inputField">
     <div class="flex items-center">of {{ maxPage }}</div>
-    <button class="btn-white" @click="increment">
+    <button class="btn-white" @click="increment" v-if="modelValue < maxPage">
       <i class="bi bi-chevron-right"></i>
     </button>
-    <button class="btn-white" @click="end">
+    <button class="btn-white" v-else disabled>
+      <i class="bi bi-chevron-right"></i>
+    </button>
+    <button class="btn-white" @click="end" v-if="modelValue != maxPage">
+      <i class="bi bi-chevron-bar-right"></i>
+    </button>
+    <button class="btn-white" v-else disabled>
       <i class="bi bi-chevron-bar-right"></i>
     </button>
   </div>
@@ -31,31 +43,31 @@ export default {
   },
   methods: {
     start() {
-        this.$emit('update:modelValue', 1);
+      this.$emit('update:modelValue', 1);
     },
     decrement() {
-        if (this.modelValue > 1) {
-            this.$emit('update:modelValue', this.modelValue - 1);
-        }
+      if (this.modelValue > 1) {
+        this.$emit('update:modelValue', this.modelValue - 1);
+      }
     },
     increment() {
-        if (this.modelValue < this.maxPage) {
-            this.$emit('update:modelValue', this.modelValue + 1);
-        }
+      if (this.modelValue < this.maxPage) {
+        this.$emit('update:modelValue', this.modelValue + 1);
+      }
     },
     end() {
-        this.$emit('update:modelValue', this.maxPage);
+      this.$emit('update:modelValue', this.maxPage);
     },
     registerValue() {
-        if (this.modelValue < 1) {
-            this.$emit('update:modelValue', 1);
-        } else if (this.modelValue > this.maxPage) {
-            this.$emit('update:modelValue', this.maxPage);
-        } else if ( !(this.modelValue >= 1 && this.modelValue <= this.maxPage) || isNaN(Number(this.modelValue)) ) {
-            this.$emit('update:modelValue', 1);
-        } else {
-            this.$emit('update:modelValue', this.$refs.inputField.value);
-        }
+      if (this.modelValue < 1) {
+        this.$emit('update:modelValue', 1);
+      } else if (this.modelValue > this.maxPage) {
+        this.$emit('update:modelValue', this.maxPage);
+      } else if ( !(this.modelValue >= 1 && this.modelValue <= this.maxPage) || isNaN(Number(this.modelValue)) ) {
+        this.$emit('update:modelValue', 1);
+      } else {
+        this.$emit('update:modelValue', this.$refs.inputField.value);
+      }
     },
   },
 }
