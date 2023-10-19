540df1075c0f64cda34e055b5fa2e9fb1dda25c0
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-10-19 12:25:11 +0800

    changed the goldman logo

---
+++ src/views/Portfolio.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <PortfolioBreakdown :portfolio="portfolio"/>
+        <div class="grid grid-cols-2 lg:grid-cols-3 gap-2">
+            <div class="white-card">
+                <p class="font-semibold mt-1 mb-4 text-navy-950">
+                    Market Exposure
+                </p>
+                <DonutChart :isOverview="false" :portfolioId="portfolio.portfolioId"/>
+            </div>
+            
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import DonutChart from '../components/charts/DonutChart.vue';
+import PortfolioBreakdown from '@/components/PortfolioBreakdown.vue';
+
+export default {
+    name: 'IndividualStock',
+    components: {
+        DonutChart,
+        PortfolioBreakdown
+    },
+    props: {
+        portfolio: {
+            type: Object,
+            required: true,
+        },
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
