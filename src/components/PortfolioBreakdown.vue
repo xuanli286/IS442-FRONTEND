d3497729959bbabd6b60022c1937b734bc431af7
Germaine <tanlixuangermaine@gmail.com>
2023-10-24 18:44:25 +0800

    update PortfolioBreakdown
    might need to redo pnl calcn

--- src/components/PortfolioBreakdown.vue
+++ src/components/PortfolioBreakdown.vue
@@ -30,9 +30,7 @@
                                     class="text-xs lg:text-sm font-medium">(Symbol | Sector)</span></th>
                             <th>Date Bought</th>
                             <th>Quantity</th>
-                            <th>Current Price <br class="lg:hidden"><span class="text-xs lg:text-base">($)</span></th>
                             <th>Bought Price <br class="lg:hidden"><span class="text-xs lg:text-base">($)</span></th>
-                            <th>Allocation<br class="lg:hidden"><span class="text-xs lg:text-base">(%)</span></th>
                             <th>Quantity Change<br class="lg:hidden"><span class="text-xs lg:text-base">(%)</span></th>
                             <th>P&L <br class="lg:hidden"><span class="text-xs lg:text-base">($)</span></th>
                         </tr>
@@ -41,7 +39,7 @@
                     <tbody v-for="(transactions, stockTicker) in portfolioStocks" :key="stockTicker" class="border-b border-navy-950">
                         <template v-if="stockInfo[stockTicker]">
                             <tr v-for="(t, idx) in transactions" :key="idx">
-                                <td class="font-semibold text-left" v-if="idx==0">
+                                <td class="text-left font-semibold stockname" v-if="idx==0">
                                     {{ stockTicker }}
                                     <span
                                         class="inline-flex items-center justify-center px-2 h-6 mx-2 bg-navy-150 rounded-lg text-xs lg:text-sm font-medium">
@@ -49,20 +47,33 @@
                                     </span>
                                     <span class="text-xs lg:text-sm">{{ stockInfo[stockTicker].sector }}</span>
                                 </td>
+                                <td class="text-left font-semibold" v-else-if="idx == 1">
+                                    Current Price: {{ (stockInfo[stockTicker].eod).toFixed(2) }}
+                                </td>
+                                <td class="text-left font-semibold" v-else-if="idx == 2">
+                                    Allocation: {{ (t.allocation * 100).toFixed(1) }}%
+                                </td>
                                 <td v-else></td>
-                            
                                 <td>{{ new Date(t.dateBought).toLocaleString(undefined, {
                                         year: 'numeric', month: 'long'
                                     }) }}</td>
                                 <td>{{ t.quantity }}</td>
-                                <td>{{ (stockInfo[stockTicker].eod).toFixed(2) }}</td>
                                 <td>{{ t.stockBoughtPrice.toFixed(2) }}</td>
-                                <td>{{ t.allocation }}</td>
                                 <td>{{ getQtyChange(transactions, idx) }}</td>
                                 <td
-                                    :class="getPnL(transactions[transactions.length - 1].quantity, stockInfo[stockTicker].eod, transactions[transactions.length - 1].stockBoughtPrice) < 0 ? 'text-red-500' : 'text-green-500'"
+                                    :class="getPnL(t.quantity, stockInfo[stockTicker].stockPrices[t.dateBought], t.stockBoughtPrice) < 0 ? 'text-red-500' : 'text-green-500'"
                                 >
-                                    {{ Math.abs(getPnL(transactions[transactions.length - 1].quantity, stockInfo[stockTicker].eod, transactions[transactions.length - 1].stockBoughtPrice)) }}
+                                    {{ Math.abs(getPnL(t.quantity, stockInfo[stockTicker].stockPrices[t.dateBought], t.stockBoughtPrice)) }}
+                                </td>
+                            </tr>
+                            <tr v-if="transactions.length == 1">
+                                <td class="text-left font-semibold">
+                                    Current Price: {{ (stockInfo[stockTicker].eod).toFixed(2) }}
+                                </td>
+                            </tr>
+                            <tr v-if="transactions.length == 1">
+                                <td class="text-left font-semibold">
+                                    Allocation: {{ (transactions[0].allocation * 100).toFixed(2) }}%
                                 </td>
                             </tr>
                         </template>
@@ -122,12 +133,14 @@ export default {
                     this.portfolioStocks[stock] = sorted[stock].reverse()
                 }
 
+                console.log(portStock)
+
                 let stocks = Object.keys(portStock);
                 let eodSum = 0;
                 let ytdSum = 0;
 
                 for (let stock of stocks) {
-                    this.stockInfo[stock] = { symbol: "", sector: "", eod: 0, ytd: 0 };
+                    this.stockInfo[stock] = { symbol: "", sector: "", eod: 0, ytd: 0, stockPrices: {} };
 
                     const stockOverviewResponse = await axios.get(`http://localhost:5000/stock/${stock}/companyOverview`);
                     this.stockInfo[stock].symbol = stockOverviewResponse.data.country;
@@ -135,6 +148,14 @@ export default {
 
                     const stockPriceResponse = await axios.get(`http://localhost:5000/stockprice/monthlyprice/${stock}`);
                     const stockPriceList = stockPriceResponse.data.stockPriceList;
+
+                    let stockPrices = {}
+
+                    for (const stockValue of stockPriceList) {
+                        stockPrices[stockValue.stockDate.slice(0,7)] = stockValue["4. close"]
+                    }
+
+                    this.stockInfo[stock].stockPrices = stockPrices;
 
                     let eodPrice = stockPriceList[0]["4. close"];
 
@@ -149,11 +170,14 @@ export default {
 
                 this.percentChange = (((eodSum - ytdSum) / ytdSum) * 100).toFixed(2);
 
+                // console.log(this.stockInfo)
+
             } catch (error) {
                 console.error("An error occurred:", error);
             }
         },
         getPnL(qty, eod, boughtPrice) {
+
             return ((eod - boughtPrice) * qty).toFixed(2);
         },
         getQtyChange(trans, idx) {
@@ -178,8 +202,12 @@ h4 {
     @apply text-2xl text-navy-950 font-semibold
 }
 
+.stockname {
+    @apply md:text-base
+}
+
 td {
-    @apply py-1 text-sm
+    @apply py-1 text-xs md:text-sm
 }
 
 th {
